a40b934e94200a18706a4ad41454b5880e2cc994
RP562 <rahulparab562work@gmail.com>
2025-01-10 15:16:10 +0530

    Changes are commited

---
+++ ignition/script-python/Project_refresh/code.py
@@ -0,0 +1,12 @@
+def project_refresh(self):
+	import system
+	import system.project
+	import time
+	logger = system.util.getLogger("Logger")
+	logger.info("Script Triggered")
+	self.getSibling("Status").props.text= "Project Directory Scanning" 
+	time.sleep(2)
+	system.project.requestScan()
+	logger.info("Script Worked Succesfully")
+	time.sleep(2)
+	self.getSibling("Status").props.text= "Project Directory Scan Complete" 
